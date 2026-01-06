6bc1c3d3b755513facfa4b98a1694870e70d6a83
zainjadoon <zainulabdin.atd@gmail.com>
2026-01-06 09:50:15 +0500

    another commit

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 const app = express();
 
-app.use("/auth", proxy("http://localhost:4001"));
-app.use("/users", proxy("http://localhost:4002"));
-app.use("/doctors", proxy("http://localhost:4003"));
-app.use("/appointments", proxy("http://localhost:4004"));
+app.use("/auth", proxy("http://auth-service:4001"));
+app.use("/users", proxy("http://patient-service:4002"));
+app.use("/doctors", proxy("http://doctor-service:4003"));
+app.use("/appointments", proxy("http://appointment-service:4004"));
 
 
 app.get("/health", (_, res) => res.send("Gateway OK"));
